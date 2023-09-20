878c13517a7747c80ee3f9a9cad4378f756f5f21
Jason E. Hale <jhale@FreeBSD.org>
2023-09-20 00:49:51 -0400

    Qt6: Update to 6.6.0-beta4

--- www/qt6-webengine/files/patch-src_3rdparty_chromium_gpu_command__buffer_service_gles2__cmd__decoder.cc
+++ www/qt6-webengine/files/patch-src_3rdparty_chromium_gpu_command__buffer_service_gles2__cmd__decoder.cc
@@ -1,11 +1,11 @@
---- src/3rdparty/chromium/gpu/command_buffer/service/gles2_cmd_decoder.cc.orig	2023-04-05 11:05:06 UTC
+--- src/3rdparty/chromium/gpu/command_buffer/service/gles2_cmd_decoder.cc.orig	2023-09-11 15:16:04 UTC
 +++ src/3rdparty/chromium/gpu/command_buffer/service/gles2_cmd_decoder.cc
-@@ -3460,7 +3460,7 @@ GLES2Decoder* GLES2Decoder::Create(
-   }
- 
+@@ -3462,7 +3462,7 @@ GLES2Decoder* GLES2Decoder::Create(
+ // Fix for Qt WebEngine Crash when QSG_RHI_BACKEND=opengl
  // Allow linux to run fuzzers.
--#if BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER) || BUILDFLAG(IS_LINUX)
-+#if BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_BSD)
+ #if BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER) || BUILDFLAG(IS_LINUX) || \
+-    BUILDFLAG(IS_WIN)
++    BUILDFLAG(IS_WIN) || BUILDFLAG(IS_BSD)
    return new GLES2DecoderImpl(client, command_buffer_service, outputter, group);
  #else
    LOG(FATAL) << "Validating command decoder is not supported.";
